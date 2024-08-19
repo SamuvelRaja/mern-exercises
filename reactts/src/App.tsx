ceebd2bdea0df91e36da15821d8b72d8c0aca1eb
SamuvelRaja <josephsamuvel001@gmail.com>
2024-08-19 22:58:39 +0530

    fixed type errors

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -4,7 +4,7 @@ import Autocomplete from './components/Autocomplete'
 
 function App() {
   
-  const getRecipes= async (query:string)=>{
+  const getRecipes= async (query:string):Promise<[]>=>{
       const res=await fetch(`https://dummyjson.com/recipes/search?q=${query}`)
       if(!res.ok){
         throw new Error('cant fetch respices')
--- reactts/src/components/Autocomplete.tsx
+++ reactts/src/components/Autocomplete.tsx
@@ -1,20 +1,25 @@
 import { useState } from "react"
 
+interface autoProps {
+  getRec:(query:string)=>Promise<[]>
+}
 
-const Autocomplete = ({getRec}:(query:string)=>any) => {
-    const[input, setinput]=useState(null)
-    const[loading, setLoading]=useState(null)
-    const[error, setError]=useState(null)
+const Autocomplete= ({getRec}:autoProps) => {
+    const[input, setinput]=useState<string>("")
+    const[loading, setLoading]=useState<boolean|null>(null)
+    const[error, setError]=useState<boolean|null>(null)
     const[suggestion, setsuggestion]=useState([])
 
-    const handleInput=(e)=>{
-      setinput()
+    const handleInput=(val:string)=>{
+      setinput(val)
     }
   return (
     <div>
-        <input type="text"  onChange={} />
+        <input type="text" value={input} onChange={(e)=>{handleInput(e.target.value)}} />
         <ul>
-          
+          {
+            loading&&<>loading..</>
+          }
         </ul>
     </div>
   )
